b4bed4356e5e87e88108276d8de848647444a126
DnyaneshKamthe <dnyaneshkamthe6@gmail.com>
2024-09-18 20:03:45 +0530

    modified test file

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,22 @@
 const { app } = require("../index")
 const { getAllEmployees, getEmploeeById} = require("../controllers/index")
+const request = require('supertest')
+const { app } = require("../index")
 
 let http = require("http")
 
-jest.mock("../controllers",()=>{
+jest.mock("../controllers",()=>({
     ...jest.requireActual("../controllers"),
     getAllEmployees : jest.fn(),
     getEmploeeById :jest.fn()
+}))
+
+let server;
+beforeAll((done) => {
+    server = http.createServer(app);
+    server.listen(3001, done)
+})
+
+afterAll((done) => {
+    server.close(done);
 })
